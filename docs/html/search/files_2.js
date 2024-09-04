24d05749a28da9d9793a3073d9782dc80e09a12e
Enricco Gemha <enriccogemha@gmail.com>
2024-09-04 14:07:03 -0300

    Set up Doxygen GitHub Actions deployment

---
+++ docs/html/search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['heuristic_2ecpp_0',['heuristic.cpp',['../heuristic_8cpp.html',1,'']]]
+];
---
+++ docs/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['load_5fgraph_0',['load_graph',['../base_8cpp.html#a2624bf9ab2b21423b2db2552184d5b28',1,'load_graph(const std::string &amp;filename, std::map&lt; int, int &gt; &amp;nodes, std::map&lt; std::pair&lt; int, int &gt;, int &gt; &amp;distances):&#160;base.cpp'],['../base_8h.html#a2624bf9ab2b21423b2db2552184d5b28',1,'load_graph(const std::string &amp;filename, std::map&lt; int, int &gt; &amp;nodes, std::map&lt; std::pair&lt; int, int &gt;, int &gt; &amp;distances):&#160;base.cpp']]]
+];
